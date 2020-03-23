c222fd20a1ac8b2c7f4163fbd710437c5a9255db
Agustin Carnevale <agustincarnevale@Agustins-MacBook-Pro.local>
2020-03-22 22:06:41 -0300

    generic Context creator function

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,17 +1,17 @@
 import React, {useContext} from 'react'
 import {View, Text, StyleSheet, FlatList, Button} from 'react-native'
-import BlogContext from '../context/BlogContext'
+import { Context as BlogContext } from '../context/BlogContext'
 
 const IndexScreen = ()=>{
 
-    const {data, addBlogPost} = useContext(BlogContext)
+    const {state, addBlogPost} = useContext(BlogContext)
 
     return (
         <View>
             <Text>INDEX SCREEN</Text>
             <Button title="Add Post" onPress={addBlogPost}/>
             <FlatList 
-                data={data}
+                data={state}
                 keyExtractor={b=>b.title}
                 renderItem={({item})=><Text>{item.title}</Text>}
             />
